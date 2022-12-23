3697a13c0298b05b0e482f3296b88201bb3eead1
puma <gustavopumachagua.com>
2022-12-23 11:49:25 -0500

    DS

---
+++ course_python_data_science/myfuncs.py
@@ -0,0 +1,16 @@
+def printMe(msg="No message was supplied"):
+  """
+  This function prints a msg supplied by the caller
+  In case no message is supplied from the caller it prints "No message was supplied"
+  """
+  print(msg)
+
+
+
+def printList(L = []):
+  """
+  This function prints the list elements
+  The input is a list and defult is empty list
+  """
+  for x in L:
+    print(x)
